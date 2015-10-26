ff989dba2b94f64bbaec12093bd6a180ff483a7e
Robert Skwiat <robert@aadmins-mbp.mediabrix.nyc>
2015-10-26 14:52:40 -0400

    adding in boilerplate, learning models

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+		port: 9999
+  });
+});
+
+gulp.task('default', ['connect']);
